255ac4a25fcbda167122af0812ed8934da5e71e1
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-11-11 01:32:12 +0000

    Trial

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <>
+      <button className="button">Button</button>
+      
+    </>
+  );
+}
+
+export default App;
