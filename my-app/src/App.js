8988ddf258f3874035ac1a33a62029ae472d37ff
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-11-12 14:48:02 +0000

    Molecules

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 
 
+   
+
+
 
 
 function App() {
   return (
     <>
-      
+     
       
     </>
   );
---
+++ my-app/src/components/molecules/NavigationBar/NavigationBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavTitle from './components/atoms/nav-title';
+import NavLinks from './components/atoms/nav-links';
+import NavBurgerIcon from '../../atoms/nav-burgerIcon';
+
+
+
+function NavigationBar() {
+    return (
+        <>
+        <NavTitle /> 
+        <NavLinks />
+        <NavBurgerIcon />
+             
+        
+        
+      </>
+    );
+  }
+  
+  export default NavigationBar;
+  
